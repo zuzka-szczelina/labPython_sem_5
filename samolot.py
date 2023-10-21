cc749175c5986e13f71a269dc68ea7cd3ba7e6b9
unknown <zuzka@mail.com>
2023-10-21 10:52:51 +0200

    samolot.py -> prostsze oznaczenia, ulepszenie funkcji, brakuje zmiany tiltu na podstawie zmiany orientacji

--- samolot.py
+++ samolot.py
@@ -2,25 +2,39 @@ from time import sleep
 import random
 import numpy as np
 #to wszystko do inputu potem
-proper_orientation = 0 #range 0-359 deg
-current_orientation = 0 #range 0-359 deg
-current_tilt = 0 #range -max_tilt: max_tilt [deg]
-max_tilt = 30 #[deg]
-max_tilt_induced_orientation_change = 10 #[deg]
-max_turbulence_tilt = 3 #[deg]
-max_orintation_correction = 10
+pr_or = 0 #proper_orientation  range 0-359 deg
+cur_or = 0 #current_orientation range 0-359 deg
+cur_til = 0 #current_tilt  range -max_tilt: max_tilt [deg]
+max_til = 30 #[deg]
+max_til_or_ch = 10 #max_tlt_induced_orientation_change   [deg]
+max_tur_til = 3 #max_turbulence_tilt[deg]
+max_or_cor = 10 #max_orintation_correction
 
 
 while True:
+    #to pewnie się da jakoś krócej napisać
+    tur_til = (random.gauss(0, 1/3*max_tur_til))
+    while abs(tur_til)>max_tur_til:
+        tur_til = (random.gauss(0, 1 / 3 * max_tur_til))
 
-    turbulence_tilt = (random.gauss(0, 1/3*max_turbulence_tilt))
-    current_tilt = (current_tilt + turbulence_tilt if abs(current_tilt + turbulence_tilt)<max_tilt else max_tilt*np.sign(current_tilt + turbulence_tilt) )
-    orientation_change = (max_tilt_induced_orientation_change/(max_tilt**2))*(current_tilt**2)*np.sign(current_tilt)
+    cur_til = (cur_til + tur_til if abs(cur_til + tur_til)<max_til else max_til*np.sign(cur_til + tur_til) )
+    #orientation_change:
+    or_ch = (max_til_or_ch/(max_til**2))*(cur_til**2)*np.sign(cur_til)
+    #orientation_before_correction:
+    or_bef_cor = cur_or + or_ch
+    #orientation_error
+    or_err = or_bef_cor - pr_or
+    #orientation_correction:
+    or_cor = (-max_or_cor*np.sin(np.pi/4*or_err/max_or_cor*1.1) if abs(or_err)<max_or_cor*1.1 else max_or_cor*(-np.sign(or_err)) )
+    cur_or = or_bef_cor + or_cor
 
-    orientation_before_correction = current_orientation + orientation_change
-    orientation_error = proper_orientation-orientation_before_correction
-    orientation_correction = (-orientation_error if abs(orientation_error)<max_orintation_correction else max_orintation_correction*(-np.sign(orientation_error)) )
-    current_orientation = orientation_before_correction + orientation_correction
-
-    print(f"orientation change = {orientation_change}\n orientation before correction = {orientation_before_correction}\n current orientation = {current_orientation}",)
-    sleep(2)
+    print(
+          f"tur_til = {tur_til}\n"
+          f"cur_til = {cur_til}\n"
+          f"orientation change = {or_ch}\n"
+          f"or_bef_cor = {or_bef_cor}\n"
+          f"or_err = {or_err}\n"
+          f"or_cor ={or_cor}\n"
+          f"current orientation = {cur_or}\n"
+         )
+    sleep(20)
